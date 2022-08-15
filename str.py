faf9f8f910fa21e460a01a3e58bf3b340008d58b
krupadav <107058808+krupadav@users.noreply.github.com>
2022-08-15 19:25:57 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,10 @@
+s = 'Hello this is string'
+print('s:', s)
+print('s.upper() gives:', s.upper())
+print('s.lower gives:', s.lower())
+print('s.capitalize gives:', s.capitalize())
+print('s.title gives:', s.title())
+print('s.casefold gives:', s.casefold())
+print('s.swapcase gives:', s.swapcase())
+print('s.split gives:', s.split())
+print('s.splitlines gives:', s.splitlines())
